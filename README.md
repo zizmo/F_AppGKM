afbdb4fc7f3872b84514666c11d571464bbb28c9
zizmo <aziz.workplace28@gmail.com>
2025-12-15 13:40:25 +0700

    Test Git

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # F_AppGKM
+
+Aplikasi Flutter untuk skripsi (Kantin GKM FILKOM UB) yang terhubung ke Firebase Realtime Database.
---
+++ android/app/src/main/kotlin/com/example/f_appgkm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.f_appgkm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
